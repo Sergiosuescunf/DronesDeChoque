12b55666e6e8ac5d2fba4b4b0a6ecc91e211a1bb
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2024-01-25 12:18:59 +0100

    Modify gym wrapper to allow the use of arbitrary agents in the simulation via their ID

--- CasaEntreno/CasaGymEnv.py
+++ CasaEntreno/CasaGymEnv.py
@@ -60,6 +60,9 @@ class CasaGymEnv(gym.Env):
         self.obs_buffer = []
         self.rew_buffer = []
         self.last_actions = np.zeros((len(self.drone_ids), 3), dtype=np.float32)
+        self.last_observations = np.zeros((self.n_drones, self.n_obs), dtype=np.float32)
+        self.last_rewards = np.zeros(self.n_drones, dtype=np.float32)
+
         self.last_actions[:, 2] = 0.1 # Set all drones to be alive by default
 
         # Create utility variables for termination
@@ -75,8 +78,6 @@ class CasaGymEnv(gym.Env):
         self.dones[:] = False
         self.crashed[:] = False
         self.states = np.zeros((self.n_drones, 4), dtype=np.float32)
-        # Get observation from Unity environment
-        observation = self._get_obs()
         # Reset utility variables for reward computation
         for id in self.drone_ids:
             self.grids[id].clean_grid()
@@ -86,44 +87,55 @@ class CasaGymEnv(gym.Env):
         self.action_buffer = []
         self.obs_buffer = []
         self.rew_buffer = []
-        # Reset last action
+        # Reset last X buffers
         self.last_actions = np.zeros((len(self.drone_ids), 3), dtype=np.float32)
         self.last_actions[:, 2] = 0.1
+        self.last_observations = np.zeros((self.n_drones, self.n_obs), dtype=np.float32)
+        self.last_rewards = np.zeros(self.n_drones, dtype=np.float32)
         self.action = self.behavior_spec.action_spec.random_action(n_agents=1)
         # Return observation and info
+        for id in self.drone_ids:
+            self.last_observations[id] = self._get_obs_id(id)
         infos = self._get_info()
 
-        return observation, infos
+        return self.last_observations[id], infos[id]
 
-    def step(self, actions: NDArray[np.float32]) -> Tuple[NDArray[np.float32], float, bool, Dict[str, Any]]:
+    def step(self, dummy_action: NDArray[np.float32]) -> Tuple[NDArray[np.float32], float, bool, Dict[str, Any]]:
+        '''
+        This methods assumes you already called set_action_id() for each drone, so the action is already set in the Unity environment
+        if you want to use this method, you should call set_action_id() for each drone before calling step()
+        dummy_action is not used, it is only here to comply with the gym interface
 
-        # Set each drone action
-        for id in self.drone_ids:
-            # Add drone state 0.2 = Alive, 0.1 = Dead
-            if self.crashed[id]:
-                action = np.append(actions[id,:], [0.1])
-            else: 
-                action = np.append(actions[id,:], [0.2])
-            
-            self.last_actions[id,:] = action
-            
-            # Pass action to Unity environment
-            self.action.add_continuous(action.reshape((1,3)))
-            self.unity_env.set_action_for_agent(self.behavior_name, id, self.action)  
+        It returns the observation, reward, done and info for the first drone to comply with the gym interface
+        nonetheless, the observations, rewards, dones and infos for all drones are updated and can be accessed through the class variables
+        '''
 
-        # Step all active drones      
+        # Step all active drones in Unity environment     
         self.unity_env.step()              
 
-        # Get observation from Unity environment
-        observations = self._get_obs()
-        # Check termination criteria
-        dones = self.check_termination()
-        # Compute reward
-        rewards = self.compute_reward()
-        # Get info
-        infos = self._get_info(observations)
+        # Get observations from Unity environment
+        for id in self.drone_ids:
+            self.last_observations[id] = self._get_obs_id(id)
+            self.dones[id] = self._check_termination(id)
+            self.last_rewards[id] = self._compute_reward(id)
 
-        return observations, rewards, dones, infos
+        # Get info
+        infos = self._get_info(self.last_observations)
+
+        return self.last_observations, self.last_rewards, self.dones, infos
+    
+    def set_action_id(self, id, action):
+        # Add drone state 0.2 = Alive, 0.1 = Dead
+        if self.crashed[id]:
+            action = np.append(action, [0.1])
+        else: 
+            action = np.append(action, [0.2])
+        
+        self.last_actions[id] = action        
+        # Pass action to Unity environment
+        self.action.add_continuous(action.reshape((1,3)))
+        self.unity_env.set_action_for_agent(self.behavior_name, id, self.action)
+        
 
     def render(self, mode='human') -> NDArray[np.float32]:
         pass
@@ -173,17 +185,57 @@ class CasaGymEnv(gym.Env):
             rewards[id] = reward - penalty
 
         return rewards
+    
+    def _compute_reward(self, id=0):
+        # Weights for rewards and penalties
+        w_forward = 10.0
+        w_explore = 200.0
+        w_grid = 1.0
+        w_distance = 20.0
+        w_crashed = 50.0
+
+        # Reward for moving forward
+        forward_rew = w_forward * self.last_actions[id][1]
+
+        # Update explored zones
+        zone = self._compute_current_zone(self.states[id][0], self.states[id][2]) # PosX, PosZ
+        if zone not in self.explored_zones[id]:
+            self.explored_zones[id].append(zone)
+            
+        # Compute exploration reward
+        explore_rew = w_explore * (len(self.explored_zones[id]) - 1)
+
+        # Compute grid reward
+        self.grids[id].update(self.states[id][0], self.states[id][2])
+        grid_rew = w_grid * (self.grids[id].puntuation())
+
+        # Get front distance to obstacles
+        dist_center = self.decision_steps[id][0][8][1]
+
+        # Compute distance penalty
+        dist_penalty = self._distance_penalty(dist_center, w_proximity_penalty=w_distance)
+        crash_penalty = w_crashed * self.crashed[id]
+
+        # Compute reward
+        reward = forward_rew + explore_rew + grid_rew
+        # Compute penalty
+        penalty = dist_penalty + crash_penalty
+        # Compute final reward
+        reward_id = reward - penalty
+        self.last_rewards[id] = reward_id
+
+        return reward_id   
+    
 
     
-    def check_termination(self):
-        for id in self.drone_ids:
-            if self.states[id][3] == 0: # Has crashed
-                self.dones[id] = True
-                self.crashed[id] = True
-            if self.steps >= self.max_steps:
-                self.dones[:] = True
+    def _check_termination(self, id=0):
+        if self.states[id][3] == 0: # Has crashed
+            self.dones[id] = True
+            self.crashed[id] = True
+        if self.steps >= self.max_steps:
+            self.dones[id] = True
         
-        return self.dones
+        return self.dones[id]
     
     def _get_obs(self):
         # Get decision steps from Unity environment
@@ -210,6 +262,32 @@ class CasaGymEnv(gym.Env):
             observation[id,:] = obs_id
 
         return observation
+    
+    def _get_obs_id(self, id):
+        # Get decision steps from Unity environment
+        self.decision_steps, self.terminal_steps = self.unity_env.get_steps(self.behavior_name)
+        
+        # Get each drone observation (laser measures)
+        self.states[id] = self.decision_steps[id][0][9]
+                    
+        # Get laser info from Unity environment
+        laser_measures = np.atleast_2d([])
+        for i in range(7):
+            laser = self.decision_steps[id][0][i]
+            reading = np.atleast_2d([laser[21], laser[17], laser[13], laser[9], laser[5], laser[1], laser[3], laser[7], laser[11], laser[15], laser[19]])
+            laser_measures = np.concatenate((laser_measures, reading), axis=1)
+                
+        height_measure = np.atleast_2d([self.decision_steps[id][0][7][1]])
+        #laser_measures = self._normalize_laser_measures(laser_measures)
+        laser_measures = np.concatenate((laser_measures, height_measure), axis=1)
+
+        # Create observation vector
+        obs_id = (laser_measures.flatten()).reshape(1, -1)
+        # Log last observation for this id
+        self.last_observations[id,:] = obs_id
+
+        return obs_id
+
     
     def _get_info(self, obs = None):
         # Log steps
