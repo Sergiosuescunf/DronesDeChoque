9543785cc5a54b2d58f80ed7b7f2dff9405cfac3
Alvaro Belmonte Baeza <alvaro.bb.7@gmail.com>
2024-01-25 12:20:23 +0100

    implement evaluation loop by hand instead of using built-in method

--- CasaEntreno/test_sb3.py
+++ CasaEntreno/test_sb3.py
@@ -29,7 +29,7 @@ def main():
     parser.add_argument('--seed', type=int, default=SEED, help='Random seed')
     parser.add_argument('--device', type=str, default='auto', help='Device for running training (cpu, cuda or auto)')
     parser.add_argument('--n_test_episodes', type=str, default=10, help='Number of episodes for testing')
-    parser.add_argument('--saved_model_path', type=str, required=True, help='Path to a trained model')
+    parser.add_argument('-p', '--saved_model_path', type=str, required=True, help='Path to a trained model')
     # Get arguments
     args = parser.parse_args()
     env_name = args.env
@@ -42,22 +42,27 @@ def main():
     # Load Unity Environment
     channel = EngineConfigurationChannel()
     channel.set_configuration_parameters(height=1024, width=1024)
-    unity_env = UnityEnvironment(file_name=env_name, seed=seed, no_graphics=not render, side_channels=[])
+    unity_env = UnityEnvironment(file_name=env_name, seed=seed, no_graphics=not render, side_channels=[channel])
     unity_env.reset()
     time.sleep(5)
     # Wrap Unity environment in a gym wrapper
-    # env = CasaGymEnv(unity_env=unity_env, seed=seed)
-    vec_env = make_vec_env(lambda: make_env(unity_env, seed), n_envs=n_envs)
+    env = CasaGymEnv(unity_env=unity_env, n_drones=1, seed=seed)
 
     # Load model
-    model = PPO.load(model_path, vec_env)
+    model = PPO.load(model_path, env=env)
 
     # Evaluate model
-    mean_reward, std_reward = evaluate_policy(model, vec_env, n_eval_episodes=n_test_episodes)
-    print('Mean reward: ' + str(mean_reward))
-    print('Std reward: ' + str(std_reward))
+    for episode in range(n_test_episodes):
+        print("Episode: ", episode)
+        obs, _ = env.reset()
+        done = False
+        while not done:
+            action, _states = model.predict(obs.reshape(1,-1), deterministic=True)
+            env.set_action_id(0, action)
+            obs, rewards, done, info = env.step(action)
+            env.render()
 
-    vec_env.close()
+    env.close()
 
     
 
